791df1d22a3482f2f555c69d321cc3b23a7f8db8
diseoweb2019 <52786731+diseoweb2019@users.noreply.github.com>
2019-07-11 09:23:05 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+/* jQuery Method Calls
+  You can still use the old jQuery plugin method calls.
+  But you won't be able to access instance properties.*/
+/*$('.carousel').carousel('methodName');
+$('.carousel').carousel('methodName', paramName);*/
+
+/* JS */
+/*document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+var instance = M.Carousel.getInstance(elem);
+instance.next();*/
